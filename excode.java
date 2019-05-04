fd9865a3e4dc9941a5e6181e54a289f5b7ee0e49
achmad akbar <43064041+wc9999@users.noreply.github.com>
2019-05-04 09:38:10 +0700

    Add files via upload

---
+++ excode.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abs;
+
+/**
+ *
+ * @author hp Compaq 6530B
+ */
+public class excode {
+    public static void main(String[] args){
+    abs utama = new abs();
+    utama.setVisible(true);
+    
+        
+    
+        
+    
+    }
+    
+}
